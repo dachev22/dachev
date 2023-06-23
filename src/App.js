7f6fa6e6bae6d755e7bc2bda7577dfc9b7198e18
dachev22 <120454508+dachev22@users.noreply.github.com>
2023-06-23 21:42:11 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React  from "react";
+import {BrowserRouter} from "react-router-dom";
+import Routing from "./Routing";
+function App() {
+
+    return (
+      <BrowserRouter>
+          <Routing/>
+      </BrowserRouter>
+  );
+}
+
+export default App;
